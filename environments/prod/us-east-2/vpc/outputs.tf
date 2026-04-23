62ecbaf92bb99aa50145c4b8258c8416d0a76e4e
alfie.fielder <alfie.fielder@hotmail.co.uk>
2026-04-23 18:39:48 +0100

    Fix prod outputs.tf to match v1 module interface
    v1 of af_portfolio_vpc_module exposes a single route_table_id (shared
    default route table) rather than separate lan_route_table_id and
    tgw_route_table_id outputs which were added in v2.

--- environments/prod/us-east-2/vpc/outputs.tf
+++ environments/prod/us-east-2/vpc/outputs.tf
@@ -10,10 +10,6 @@ output "tgw_subnet_ids" {
   value = module.vpc.tgw_subnet_ids
 }
 
-output "lan_route_table_id" {
-  value = module.vpc.lan_route_table_id
-}
-
-output "tgw_route_table_id" {
-  value = module.vpc.tgw_route_table_id
+output "route_table_id" {
+  value = module.vpc.route_table_id
 }
